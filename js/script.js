cb6c4e3662f5bac665eca735a12f2880ab33f7d3
jeongsa96 <yahyashidiq@gmail.com>
2024-11-05 22:10:21 +0700

    progress 3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-// file javascript
+function hitung() {   
+        var jk = document.querySelector('input[name="gender"]:checked');
+        const beratBadan = document.getElementById("berat_badan").value;
+        const tinggiBadan = document.getElementById("tinggi_badan").value;        
+        var bmi = "";
+        if(jk == null) {
+            alert("pilih jenis kelamin");
+        } else {            
+            tinggi = tinggiBadan / 100;            
+            t = parseFloat(tinggi**2);             
+            bmi = parseFloat(beratBadan / t);
+
+            if (bmi < 18.5) {
+                  
+            } else if (bmi >= 18.5 && bmi < 25 ) {
+                
+                document.getElementById("result").innerHTML = bmi;
+            } else if (bmi >= 25 && bmi < 30 ) {
+
+            } else {
+
+            }
+        }
+    }
+    
+function resetForm() {
+    document.getElementById("berat_badan").value = "";
+    document.getElementById("usia").value = "";
+    document.getElementById("tinggi_badan").value = "";
+}    
